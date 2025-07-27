b90486d0b9e9a04286e40c01061345690641207c
Jens Timmerman <281523+JensTimmerman@users.noreply.github.com>
2025-07-27 16:29:44 +0200

    fix uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
-DAEMON_NAME=${SCRIPT_DIR##*/}
+DAEMON_NAME=$(ls ${SCRIPT_DIR}/*.py)
 
 rm /service/$DAEMON_NAME
-kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
-kill $(pgrep -f 'supervise $DAEMON_NAME')
+pkill -f "python $SCRIPT_DIR/$DAEMON_NAME"
+DAEMON_NAME=${SCRIPT_DIR##*/}
+pkill -f "supervise $DAEMON_NAME"
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
