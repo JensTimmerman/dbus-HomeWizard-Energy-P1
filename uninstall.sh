2fb41ec4529d291d9782e7222be082f508bebc5e
Jens Timmerman <281523+JensTimmerman@users.noreply.github.com>
2025-07-28 12:14:06 +0200

    fix  uninstall.sh good this time

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
 DAEMON_NAME=$(ls ${SCRIPT_DIR}/*.py)
+RC_FILE_PATH=/data/rc.local
+STARTUP=$SCRIPT_DIR/install.sh
 
-rm /service/$DAEMON_NAME
 pkill -f "python $SCRIPT_DIR/$DAEMON_NAME"
 DAEMON_NAME=${SCRIPT_DIR##*/}
+rm /service/$DAEMON_NAME
 pkill -f "supervise $DAEMON_NAME"
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
+
+sed -i "\~$STARTUP~d" $RC_FILE_PATH
