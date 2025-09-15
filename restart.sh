4bb25fc9c231a3b1c37edd15fdb419824ec746d9
Jens Timmerman <281523+JensTimmerman@users.noreply.github.com>
2025-09-15 12:06:28 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DAEMON_NAME=$(ls ${SCRIPT_DIR}/*.py)
 
-pkill -f "python $SCRIPT_DIR/$DAEMON_NAME"
+pkill -f "python $DAEMON_NAME"
